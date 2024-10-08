0adbc32504f1d78e95d5da6b6fa9e38c670ddfa7
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-10-08 07:15:40 +0000

    Star Sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,15 +5,54 @@ import java.util.Scanner;
 public class App
 {
     public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
         printTriangle(4);
+        printRectangle(17, 3);
+        printSquare(4);
     }
 
-    public static void printTriangle(int size) {
-     for (int st = 0; size > st; st++) {
-        for(int a = 0; st+1 > a; a++) {
+
+        public static void printStars(int number) {
+           int st = 0;
+           while (st < number) {
             System.out.print("*");
-       }
-       System.out.println();
+            st++;
+           }
+           System.out.println();
+        }
+        public static void printSquare(int size) {
+        System.out.println();
+         for (int st = 0; size > st; st++) {
+            for(int a = 0; size > a; a++) {
+                System.out.print("*");
+           }
+           System.out.println();
+        }
     }
-}
-}
+    
+    
+        public static void printRectangle(int width, int height) {
+        System.out.println();
+         for (int st = 0; height > st; st++) {
+            for(int a = 0; width > a; a++) {
+                System.out.print("*");
+           }
+           System.out.println();
+        }
+    }
+
+    
+    
+        public static void printTriangle(int size) {
+        System.out.println();
+         for (int st = 0; size > st; st++) {
+            for(int a = 0; st+1 > a; a++) {
+                System.out.print("*");
+           }
+           System.out.println();
+        }
+    }
+    }
+
