17d2800b2cabd2819a4ead4ed5538e5bb3b4337b
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-10-08 06:47:00 +0000

    Printing a triangle

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,22 +5,15 @@ import java.util.Scanner;
 public class App
 {
     public static void main(String[] args) {
-        printRectangle(17, 3);
+        printTriangle(4);
     }
 
-    public static void printRectangle(int width, int height) {
-     for (int st = 0; height > st; st++) {
-        for(int a = 0; width > a; a++) {
+    public static void printTriangle(int size) {
+     for (int st = 0; size > st; st++) {
+        for(int a = 0; st+1 > a; a++) {
             System.out.print("*");
        }
        System.out.println();
     }
 }
 }
-
-
- // you can print one star with the command
-        // System.out.print("*");
-        // call the print command n times
-        // in the end print a line break with the comand
-        // System.out.println("");
