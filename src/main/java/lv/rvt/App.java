9500cfa5f3930addf28c9aaaeba748a4278e0f54
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-10-22 07:51:28 +0000

    Print in range

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,32 +5,28 @@ import java.util.*;
 public class App
 
 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>();
-
-
-        System.out.println("Enter numbers (enter -1 to stop):");
-        while (true) {
-            int input = scanner.nextInt();
-            if (input == -1) {
-            break;
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int number: numbers) {
+            if (lowerLimit <= number && number <= upperLimit) {
+                System.out.println(number);
             }
-            numbers.add(input);
         }
+    }
 
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
 
-        System.out.println("From where? ");
-        int startIndex = scanner.nextInt();
-        System.out.println("To where? ");
-        int endIndex = scanner.nextInt();
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
 
-
-        System.out.println("Numbers in the range:");
-        for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println(numbers.get(i));
-            }
-
-        scanner.close();
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+ 
     }
 }
