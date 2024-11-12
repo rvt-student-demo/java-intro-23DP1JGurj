d8fae95f35d5af21294bfb31377df34fa019cc53
Jegors Gurjevs <a230245jg@rvt.lv>
2024-11-12 09:27:23 +0200

    Part1 The class template

---
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -0,0 +1,16 @@
+package lv.rvt;
+
+public class PaymentCard {
+    
+    private double balance;
+    
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+    
+    public String toString() {
+        return "The card has a balance of " + balance + " euros";
+    }
+}
+
+
