78f04a61a7e3fc45d9687387c6d67d9962cb5ed0
Jegors Gurjevs <a230245jg@rvt.lv>
2024-11-12 09:28:44 +0200

    Part2 Making transactions

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -1,16 +1,25 @@
 package lv.rvt;
 
 public class PaymentCard {
-    
     private double balance;
-    
+
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
     }
-    
+
+    public void eatAffordably() {
+        if (balance >= 2.60) {
+            balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (balance >= 4.60) {
+            balance -= 4.60;
+        }
+    }
+
     public String toString() {
         return "The card has a balance of " + balance + " euros";
     }
 }
-
-
