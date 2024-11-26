12dae12ca28cacd308ec09d53a98ee712c7c6727
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-11-26 07:06:06 +0000

    Constructor Overload

---
+++ src/main/java/lv/rvt/Product.java
@@ -0,0 +1,30 @@
+package lv.rvt;
+
+public class Product {
+    private String name;
+    private String location;
+    private int weight;
+
+    public Product(String name, String location, int weight) {
+        this.name = name;
+        this.location = location;
+        this.weight = weight;
+    }
+
+    public Product(String name) {
+        this(name, "shelf", 1);
+    }
+
+    public Product(String name, String location) {
+        this(name, location, 1);
+    }
+
+    public Product(String name, int weight) {
+        this(name, "shelf", weight);
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (" + this.weight + " kg) can be found from the " + this.location;
+    }
+}
