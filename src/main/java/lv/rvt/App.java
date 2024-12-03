41dcf1d3f44714d3f6fcf78bee3351417e6097db
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-12-03 10:48:27 +0000

    Lietotāja reģistrācija, informācija saglābājas csv failā

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,9 +6,22 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person person = new Person("Jegors", 16, 81, 196);
+        // Izveidojam iespēju programmas lietotājam ievadīt personas datus un saglabāt csv failā ar iepriekš izveidoto metodi
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
+        int age = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter your weight: ");
+        int weight = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter your height: ");
+        int height = Integer.valueOf(scanner.nextLine());
+
+        Person person = new Person(name, age, weight, height);
 
         PersonManager.addPerson(person);
+        System.out.println("Paldies par reģistrāciju!");
     }
     public static void modify(Person person) {
         person.setName("Modified!");
