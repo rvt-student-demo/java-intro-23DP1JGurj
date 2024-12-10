1aa8c245f484fb3ccf72a2ed6d6ccbeb00c0530c
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-12-10 10:25:30 +0000

    Health Station (Weighing people (Part 1))

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,53 +1,19 @@
-package lv.rvt; 
-
-import java.io.BufferedWriter;
+package lv.rvt;
 import java.nio.file.StandardOpenOption;
-import java.io.BufferedReader; 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.util.*;
 
-public class App { 
-    public static void main(String[] args) throws Exception { 
-        Scanner scanner = new Scanner(System.in);
+public class App {
+    public static void main(String[] args) {
+        // Example main program for the first section of the exercise
 
-        System.out.println("Sveiki, lūdzu izvēlies komandu: ");
-        System.out.println("show - parādīt visas personas");
-        System.out.println("add - pievienot personu");
-        System.out.println("exit - iziet no programmas");
-        String userCommand = scanner.nextLine();
-        
-        while(userCommand.equals("exit") == false) {
-            if (userCommand.equals("show")) {
-                System.out.println("Show all persons: ");
-                ArrayList<Person> persons = PersonManager.getPersonList();
-                System.out.println("----------------------------------------------------");
-                System.out.println("|  Name  | Age | Weight | Height | Body mass index |");
-                System.out.println("----------------------------------------------------");
-                for (Person person1: persons) {
-                    System.out.printf("| %-7s| %-6d| %-7d| %-7d| %-16.2f|", person1.getName(), person1.getAge(), person1.getWeight(), person1.getHeight(), person1.bodyMassIndex());
-                    System.out.println();
-                }
-                System.out.println("----------------------------------------------------");
-            
-            } else if (userCommand.equals("add")) {
-                System.out.println("Enter your name: ");
-                String name = scanner.nextLine();
-                System.out.println("Enter your age: ");
-                int age = Integer.valueOf(scanner.nextLine());
-                System.out.println("Enter you weight: ");
-                int weight = Integer.valueOf(scanner.nextLine());
-                System.out.println("Enter your height: ");
-                int height = Integer.valueOf(scanner.nextLine());
-    
-                Person person = new Person(name, age, weight, height);
-    
-                PersonManager.addPerson(person);
-                System.out.println("Person added");
-            }
-            userCommand = scanner.nextLine();
-        }
-        System.out.println("Bye!");
+        HealthStation childrensHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan", 1, 7, 110);
+        Person peter = new Person("Peter", 33, 85, 176);
+
+        System.out.println(ethan.getName() + "'s" + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + "'s" + " weight: " + childrensHospital.weigh(peter) + " kilos");
     }
-    public static void modify(Person person) {
-        person.setName("Modified!");
-    } 
 }
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -5,40 +5,64 @@ public class Person {
     private int age;
     private int weight;
     private int height;
+    private int shelf;
 
-    // All args constructor
+    // All args class constructor
     public Person(String name, int age, int weight, int height) {
-        this.name = name != null ? name : "Unknown";
-        this.age = age >= 0 ? age : 0; // Ensure non-negative age
-        this.weight = weight >= 0 ? weight : 0;
-        this.height = height >= 0 ? height : 0;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.name = name;
     }
 
-    // Constructor with only a name
+    public Person(String name, String age, String weight, String height) {
+        this.name = name;
+        this.age = Integer.valueOf(age);
+        this.weight = Integer.valueOf(weight);
+        this.height = Integer.valueOf(height);
+    }
+
+    // Second constructor with only one parameter
     public Person(String name) {
-        this(name, 0, 0, 0); // Default other fields to 0
+        this(name, 0, 0, 0);
     }
 
-    // Getters
+    public int returnAge() {
+        return this.age;
+    }
+
+    public boolean isOfLegalAge() {
+        return this.age <= 18;
+    }
+
+    // Getter un setter
     public String getName() {
         return name;
     }
 
     public int getAge() {
-        return age;
+        return this.age;
+    }
+
+    public int getHeight() {
+        return this.height;
     }
 
     public int getWeight() {
         return weight;
-    }
+    } 
 
-    public int getHeight() {
-        return height;
-    }
-
-    // Setters
     public void setName(String newName) {
         this.name = newName;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", age " + this.age + " years";
+    }
+    
+    public void printPerson() {
+        System.out.println(this.name + ", age " + this.age + " years");
     }
 
     public void setHeight(int newHeight) {
@@ -49,28 +73,20 @@ public class Person {
         this.weight = newWeight;
     }
 
-    // Modify the toString() method to print only the CSV-style format
-    @Override
-    public String toString() {
-        return this.name + ", " + this.age + ", " + this.weight + ", " + this.height;
-    }
-
     public double bodyMassIndex() {
-        double heightInMeters = this.height / 100.0;
-        return this.weight / (heightInMeters * heightInMeters);
+        double heigthPerHundred = this.height / 100.0;
+        return this.weight / (heigthPerHundred * heigthPerHundred);
     }
-
+    
     public void growOlder() {
-        this.age++;
+        this.age = this.age + 1;
     }
-
+    
     public void growOlder(int years) {
-        if (years > 0) {
-            this.age += years;
-        }
+        this.age = this.age + years;
     }
-
+    
     public String toCsvRow() {
         return this.name + ", " + this.age + ", " + this.weight + ", " + this.height;
     }
-}
+}   
