f46010089a7d22efe9532f1e8e04287651553f59
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-12-10 10:26:30 +0000

    Health Station (Feeding (Part 2))

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,12 +6,20 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        // Example main program for the first section of the exercise
-
         HealthStation childrensHospital = new HealthStation();
 
         Person ethan = new Person("Ethan", 1, 7, 110);
         Person peter = new Person("Peter", 33, 85, 176);
+
+        System.out.println(ethan.getName() + "'s" + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + "'s" + " weight: " + childrensHospital.weigh(peter) + " kilos");
+
+        
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+
+        System.out.println("");
 
         System.out.println(ethan.getName() + "'s" + " weight: " + childrensHospital.weigh(ethan) + " kilos");
         System.out.println(peter.getName() + "'s" + " weight: " + childrensHospital.weigh(peter) + " kilos");
--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,9 +1,13 @@
+
 package lv.rvt;
 
 public class HealthStation {
 
     public int weigh(Person person) {
-        // Return the weight of the person passed as the parameter
         return person.getWeight();
     }
-}
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+}   
