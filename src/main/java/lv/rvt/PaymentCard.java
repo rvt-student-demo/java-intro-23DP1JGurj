bad17a40da17803eafccdd259756e6c60629efa7
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2024-12-10 10:29:01 +0000

    Card Payments ("Dumb" payment card)

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -3,32 +3,40 @@ package lv.rvt;
 public class PaymentCard {
     private double balance;
 
-    public PaymentCard(double openingBalance) {
-        this.balance = openingBalance;
+    public PaymentCard(double balance) {
+        this.balance = balance;
     }
 
-    public void eatAffordably() {
-        if (balance >= 2.60) {
-            balance -= 2.60;
-        }
-    }
-
-    public void eatHeartily() {
-        if (balance >= 4.60) {
-            balance -= 4.60;
-        }
-    }
-
-    public void addMoney(double amount) {
-        if (amount > 0) {
-            balance += amount;
-            if (balance > 150) {
-                balance = 150;
-            }
-        }
+    public double balance() {
+        return this.balance;
     }
 
     public String toString() {
-        return "The card has a balance of " + balance + " euros";
+        return "The card has a balance of " + this.balance + " euros"  ;
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+    }
+    
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+    }
+
+    public void addMoney(double increase) {
+        this.balance = this.balance + increase;
+    }
+
+    public boolean takeMoney(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
