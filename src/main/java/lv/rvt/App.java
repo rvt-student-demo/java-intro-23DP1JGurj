aaf640a22d6b1b288c4edf2fa1ba61d4e576f98f
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2025-01-15 06:53:48 +0000

    Box (Exercise 2)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,12 +7,11 @@ import javax.sound.midi.Soundbank;
 import lv.rvt.connecticut.Box; 
  
 public class App { 
-    public static void main ( String[] args )
-    {
-       Box box = new Box( 2.5, 5.0, 6.0 ) ;
-  
-       System.out.println("Area: " + box.area() + " volume: " + box.volume());
-  
-       System.out.println(box);
+    public static void main( String[] args ) {
+        Box box = new Box( 2.5, 5.0, 6.0 );
+
+        System.out.println("Area: " + box.area() + " volume: " + box.volume());
+        System.out.println("topArea: " + box.getTopArea());
+        System.out.println(box);
     }
 }
