b406abd772a7a959e2667bd18eef9f79bd20375c
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2025-01-15 06:56:51 +0000

    Box (Exercise 4)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,12 +8,14 @@ import lv.rvt.connecticut.Box;
  
 public class App { 
     public static void main( String[] args ) {
-        Box box = new  Box(2.5, 5.0, 6.0) ;
+        Box box = new Box(2.5, 5.0, 6.0) ;
         Box box1 = new Box(box);
+        Box box2 = new Box(box1);
 
         System.out.println("Area: " + box.area() + " volume: " + box.volume());
         System.out.println("topArea: " + box.getTopArea());
         System.out.println(box);
         System.out.println(box1);
+        System.out.println(box.smallerBox(box2));
     }
 }
