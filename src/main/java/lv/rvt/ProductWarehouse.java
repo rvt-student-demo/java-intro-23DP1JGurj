bbb4032152666119584c390f34e02d0264fb85f5
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2025-02-06 13:56:54 +0000

    Product warehouse (Part 2)

--- src/main/java/lv/rvt/ProductWarehouse.java
+++ src/main/java/lv/rvt/ProductWarehouse.java
@@ -1,17 +1,24 @@
-
 package lv.rvt;
 
-public class ProductWarehouse extends Warehouse{
-    private String productName;
+public class ProductWarehouse extends Warehouse {
+    private String name;
+    
 
-
-    public ProductWarehouse(String productName, double capacity){
+    public ProductWarehouse(String productName, double capacity) {
         super(capacity);
-        this.productName = productName;
+        this.name = productName;
     }
 
-    public String getName(){
-        return this.productName;
+    public String getName() {
+        return this.name;
     }
 
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": balance = " +  getBalance() + ", space left " + howMuchSpaceLeft();
+    }
 }
