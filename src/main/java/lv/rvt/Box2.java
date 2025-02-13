3dfc88f87a851ed39a3b4f9cdcea017a194f3f60
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2025-02-13 12:26:16 +0000

    Interface In A Box (2 part)

---
+++ src/main/java/lv/rvt/Box2.java
@@ -0,0 +1,36 @@
+package lv.rvt;
+import lv.rvt.Book;
+import lv.rvt.CD;
+import java.util.ArrayList;
+
+public class Box2 {
+    private double capacity;
+    int items = 0;
+    double totalWeight = 0;
+
+    public Box2(int kg){
+        this.capacity = kg;
+    }
+
+    public void add(Book book){
+        if(this.capacity > book.weight()) {
+            items += 1;
+            totalWeight += book.weight();
+            this.capacity -= book.weight();
+        }
+    }
+    
+    public void add(CD cd){
+        if(this.capacity > cd.weight()) {
+            items += 1;
+            totalWeight += cd.weight();
+            this.capacity -= cd.weight();
+        }
+    }
+
+
+    public String toString() { 
+        return "Box: " + items + " items, " + "total weight " +     
+        totalWeight + " kg"; 
+    }
+}
