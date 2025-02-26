a66668006c137b7a1cf6019d7d7f857cd6af7550
23DP1JGurj <166118408+23DP1JGurj@users.noreply.github.com>
2025-02-26 07:06:42 +0000

    Interface In A Box (4 part)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,22 @@
 package lv.rvt; 
-import java.io.BufferedWriter;
-import java.nio.file.StandardOpenOption;
-import java.io.BufferedReader; 
 import java.util.*;
-
-import javax.sound.midi.Soundbank;
-
-import lv.rvt.connecticut.Box; 
  
 public class App { 
-    public static void main(String[] args) {
-        Box2 box = new Box2(10);
-
+    public static void main(String[] args){
+        boxes box = new boxes(10);
+        boxes box2 = new boxes(20);
+        
         box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
         box.add(new Book("Robert Martin", "Clean Code", 1));
         box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
-
+    
         box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
         box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
         box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
 
-        System.out.println(box);
+        box2.add(box);
+    
+        System.out.println(box2);
+    
     }
 }
